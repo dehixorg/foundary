b6d44d0fe82ae4dee16deb7c7a93198a00756de1
ambar gupta <ambargupta858@gmail.com>
2025-08-08 22:28:43 +0530

    commit

--- src/FreelancerContract.sol
+++ src/FreelancerContract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+ 
 
+ 
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
